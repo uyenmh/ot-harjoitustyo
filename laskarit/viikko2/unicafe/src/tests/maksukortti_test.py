cc37d05d43329a7ef0900577a0897f9550094ed6
Uyen <152192561+uyenmh@users.noreply.github.com>
2025-03-21 12:50:35 +0200

    Create unit tests

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,34 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 20.0)
+
+    def test_saldo_vahenee_oikein_jos_rahaa_on_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5.0)
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(2000)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_ota_rahaa_palauttaa_true_jos_rahat_riittavat(self):
+        boolean = self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(boolean, True)
+
+    def test_ota_rahaa_palauttaa_false_jos_rahat_eivat_riita(self):
+        boolean = self.maksukortti.ota_rahaa(2000)
+
+        self.assertEqual(boolean, False)
+
+    def test_str_tulostuu_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
