c07f55d06980143f09dbcacb66237a2d4520746d
Uyen <152192561+uyenmh@users.noreply.github.com>
2025-04-01 19:16:11 +0300

    add unit tests

--- src/tests/sudoku_game_test.py
+++ src/tests/sudoku_game_test.py
@@ -13,17 +13,45 @@ class TestSudokuGame(unittest.TestCase):
 
         self.assertEqual(self.app.difficulty.get(), "Hard")
 
-    def test_start_game(self):
-        pass
+    def test_start_game_at_easy(self):
+        self.app.start_game()
+
+        self.assertEqual(self.app.difficulty.get(), "Easy")
+        self.assertIsNotNone(self.app.game_root)
+        self.assertEqual(self.app.game_root.title(), "Sudoku")
+        self.assertIsInstance(self.app.puzzle, Sudoku)
+        self.assertIsInstance(self.app.solution, Sudoku)
+
+    def test_start_game_at_medium(self):
+        self.app.difficulty.set("Medium")
+
+        self.app.start_game()
+
+        self.assertEqual(self.app.difficulty.get(), "Medium")
+        self.assertIsNotNone(self.app.game_root)
+        self.assertEqual(self.app.game_root.title(), "Sudoku")
+        self.assertIsInstance(self.app.puzzle, Sudoku)
+        self.assertIsInstance(self.app.solution, Sudoku)
+
+    def test_start_game_at_hard(self):
+        self.app.difficulty.set("Hard")
+
+        self.app.start_game()
+
+        self.assertEqual(self.app.difficulty.get(), "Hard")
+        self.assertIsNotNone(self.app.game_root)
+        self.assertEqual(self.app.game_root.title(), "Sudoku")
+        self.assertIsInstance(self.app.puzzle, Sudoku)
+        self.assertIsInstance(self.app.solution, Sudoku)
 
     def test_exit_game(self):
-        pass
-
-    def test_create_sudoku(self):
         pass
     
     def test_end_game(self):
         pass
 
-    def test_check_solution(self):
+    def test_check_correct_solution(self):
+        pass
+
+    def test_check_incorrect_solution(self):
         pass
